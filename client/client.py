459cf257630d52aaff78b096a5138aeefa5afeb4
Koei <eshanshekhaji@gmail.com>
2025-08-29 19:12:49 +0530

    added crude registration system for testing on server

--- client/client.py
+++ client/client.py
@@ -1,19 +1,35 @@
 import socket
+import pickle
 
 def greet(nick: str, c: socket.socket):
     c.send(nick.encode())
 
-nick = input("Nickname: ")
+def send_reg_msg(user: str, pwd: str, c: socket.socket):
+    print("sending REG to server")
+    c.send("REG".encode())
+    response = c.recv(1024).decode()
+    if response == "OK":
+        print("server responded OK, sending user and pwd")
+        c.send(pickle.dumps((user, pwd)))
+        print(c.recv(1024).decode())
+        #login
+    else:
+        return False
 
 
-host = "127.0.0.1"
-port = 14169
-c = socket.socket()
-c.connect((host,port))
-greet(nick, c)
-message = c.recv(1024)
-print(message.decode())
-
+try:
+    host = "koei.hackclub.app"
+    port = 14169
+    c = socket.socket()
+    c.connect((host,port))
+    user = input("User: ")
+    password = input("Password: ")
+    # greet(nick, c)
+    send_reg_msg(user, password, c)
+    message = c.recv(1024)
+    print(message.decode())
+except KeyboardInterrupt:
+    c.close()
 
 
 
--- server/server.py
+++ server/server.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+import pickle
+
 
 host = "127.0.0.1"
 port = 14169
@@ -12,13 +14,29 @@ clients: list[socket.socket] = []
 nicknames: dict[socket.socket, str] = {}
 
 # def broadcast():
+import sqlite3
 
+user_db = sqlite3.connect("user.db")
+cur = user_db.cursor()
 
-def handle(client: socket.socket):
+def register_user(client: socket.socket):
+    print("sending OK")
+    client.send("OK".encode())
+    user, pwd = pickle.loads(client.recv(1024))
+    print(f"received credentials {user}-{pwd}")
+    cur.execute(f'insert into users values("{user}", "{pwd}")')
+    return user
+
+def handle(client: socket.socket, user: str):
+    print(f"{user} has connected ({client.getpeername()}).")
+    client.send("youve connected.".encode())
     while True:
         try:
             message = client.recv(1024)
             # do something with the message
+        except KeyboardInterrupt:
+            client.close()
+            return
         except:
             index = clients.index(client)
             nickname = nicknames[client]
@@ -26,19 +44,19 @@ def handle(client: socket.socket):
             nicknames.pop(client)
             print(f"{nickname} has disconnected.")
             client.close()
+            s.close()
             break
 
 
 def receive():
     while True:
         client, (addr, port) = s.accept()
-        nick = client.recv(1024).decode()
-        print(f"{nick} has connected ({addr}:{port}).")
-        nicknames[client] = "Un humain"
-
-        client.send("Vous êtes connecté au service des Communications Electroniques de la France. Bienvenue.".encode())
-        thread = threading.Thread(target=handle, args=(client,))
+        greet = client.recv(1024).decode()
+        if greet == "REG":
+            print("client has requested to register")
+            user = register_user(client)
+        thread = threading.Thread(target=handle, args=(client, user))
         thread.start()
-
+    
 receive()
 
