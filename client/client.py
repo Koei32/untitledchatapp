d0e21d5b71f4e70e94d70890c9dc627cf476c2a4
Koei <eshanshekhaji@gmail.com>
2025-08-31 01:10:48 +0530

    cleaned up client.py a bit

--- client/client.py
+++ client/client.py
@@ -1,34 +1,31 @@
 from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtGui import QFont
 import socket
 import pickle
 import sys
 
 from login_form import LoginForm
 
+font = QFont("Trebuchet", 10)
+font.setStyleStrategy(QFont.StyleStrategy.NoAntialias)
+font.setBold(True)
+
 host = "koei.hackclub.app"
 port = 14169
 c = socket.socket()
-# c.connect((host,port))
 
-def greet(nick: str, c: socket.socket):
-    c.send(nick.encode())
 
-def send_reg_msg(user: str, pwd: str, c: socket.socket):
-    c.connect((host, port))
-    print("sending REG to server")
-    c.send("REG".encode())
-    response = c.recv(1024).decode()
-    if response == "OK":
-        print("server responded OK, sending user and pwd")
-        c.send(pickle.dumps((user, pwd)))
-        print(c.recv(1024).decode())
-        #login
-    else:
-        return False
+#
+#
+# TODO: CHECK PASSWORD VALIDITY
+# TODO: STORE HASHED PASSWORDS and CHECK WITH HASH (salt and all)
+# TODO: ADD TAB SWITCHING FOR REG/LOGIN
+#
+#
 
 
 app = QApplication(sys.argv)
-
+app.setFont(font)
 window = QMainWindow()
 window.setWindowTitle("Untitled Chat App")
 login = LoginForm(c)
